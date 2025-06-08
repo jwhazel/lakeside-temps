71c86f850c8028fa83a2af6aa25fc53d76cc2f65
jwhazel <jwhazel@me.com>
2025-06-08 18:19:08 -0400

    feat: add route caching and some super basic SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,27 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint'],
+  app: {
+    head: {
+      title: 'Lakeside Swim Club Temperatures',
+      meta: [{ name: 'robots', content: 'noindex' }],
+    },
+  },
+  routeRules: {
+    '/': { isr: 86400 },
+    '/upload': { prerender: true },
+    '/api/get-temps': { isr: 86400, cors: true },
+  },
   runtimeConfig: {
     passPhrase: process.env.PASSPHRASE,
     redisURL: process.env.REDIS_URL,
     redisToken: process.env.REDIS_TOKEN,
+  },
+  nitro: {
+    vercel: {
+      config: {
+        bypassToken: process.env.VERCEL_BYPASS_TOKEN,
+      },
+    },
   },
 });
