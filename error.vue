103ee172951bed37a9c628913d0938f9b291cb73
jwhazel <jwhazel@me.com>
2025-06-08 16:28:29 -0400

    chore: format with prettier

--- error.vue
+++ error.vue
@@ -1,4 +1,4 @@
 <template>
-    <h1>404</h1>
-    <div>Page not found</div>
+  <h1 class="font-bold text-xl">404</h1>
+  <div>Page not found</div>
 </template>
--- pages/upload.vue
+++ pages/upload.vue
@@ -1,65 +1,83 @@
 <template>
-    <main class="min-h-screen min-w-screen bg-slate-400 content-center justify-center">
-        <section class="w-80 mx-auto">
-            <div class="bg-white p-10">
-                <div class="mb-5">
-                    <label class="block font-semibold" for="input-lake">Lake Temperature:</label>
-                    <input
-                        class="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                        id="input-lake" type="number" min="40" max="100" v-model="form.lakeTemp" /> °F
-                </div>
-                <div class="mb-5">
-                    <label class="block font-semibold" for="input-pool">Pool Temperature:</label>
-                    <input
-                        class="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                        id="input-pool" type="number" min="40" max="100" v-model="form.poolTemp" /> °F
-                </div>
-                <div class="mb-5">
-                    <label class="block font-semibold" for="input-pass">Passphrase:</label>
-                    <input
-                        class="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                        id="input-pass" type="text" v-model="form.passPhrase" />
-                </div>
-                <div class="mb-5">
-                    <button
-                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                        @click="submit" :disabled="status.submissionInProgress">Update</button>
-                </div>
+  <main class="min-h-screen min-w-screen bg-slate-400 content-center justify-center">
+    <section class="w-80 mx-auto">
+      <div class="bg-white p-10">
+        <div class="mb-5">
+          <label class="block font-semibold" for="input-lake">Lake Temperature:</label>
+          <input
+            class="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            id="input-lake"
+            type="number"
+            min="40"
+            max="100"
+            v-model="form.lakeTemp"
+          />
+          °F
+        </div>
+        <div class="mb-5">
+          <label class="block font-semibold" for="input-pool">Pool Temperature:</label>
+          <input
+            class="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            id="input-pool"
+            type="number"
+            min="40"
+            max="100"
+            v-model="form.poolTemp"
+          />
+          °F
+        </div>
+        <div class="mb-5">
+          <label class="block font-semibold" for="input-pass">Passphrase:</label>
+          <input
+            class="bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            id="input-pass"
+            type="text"
+            v-model="form.passPhrase"
+          />
+        </div>
+        <div class="mb-5">
+          <button
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            @click="submit"
+            :disabled="status.submissionInProgress"
+          >
+            Update
+          </button>
+        </div>
 
-                <div>{{ status.response }}</div>
-            </div>
-        </section>
-    </main>
+        <div>{{ status.response }}</div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script setup>
 const form = reactive({
-    lakeTemp: null,
-    poolTemp: null,
-    passPhrase: null
-})
+  lakeTemp: null,
+  poolTemp: null,
+  passPhrase: null,
+});
 
 const status = reactive({
-    submissionInProgress: false,
-    response: null
-})
+  submissionInProgress: false,
+  response: null,
+});
 
 const submit = async () => {
-    status.submissionInProgress = true;
+  status.submissionInProgress = true;
 
-    try {
-        await $fetch('/api/set-temps', {
-            method: 'post',
-            body: { lakeTemp: form.lakeTemp, poolTemp: form.poolTemp, passPhrase: form.passPhrase.toLowerCase() }
-        })
-        status.response = "✅ Successfully submitted temperatures"
-    }
-    catch (e) {
-        status.response = "❌ There was an error saving temperatures"
-    }
+  try {
+    await $fetch('/api/set-temps', {
+      method: 'post',
+      body: { lakeTemp: form.lakeTemp, poolTemp: form.poolTemp, passPhrase: form.passPhrase.toLowerCase() },
+    });
+    status.response = '✅ Successfully submitted temperatures';
+  } catch (e) {
+    status.response = '❌ There was an error saving temperatures';
+  }
 
-    formIsSubmitting.value = false;
-}
+  formIsSubmitting.value = false;
+};
 </script>
 
 <style>
@@ -67,12 +85,12 @@ const submit = async () => {
 /* Chrome, Safari, Edge, Opera */
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
+  -webkit-appearance: none;
+  margin: 0;
 }
 
 /* Firefox */
-input[type=number] {
-    -moz-appearance: textfield;
+input[type='number'] {
+  -moz-appearance: textfield;
 }
 </style>
